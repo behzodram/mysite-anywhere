3b5b89ba307480fbc1ac80588a46ab4b5a1be02b
Behzod <behzodmx27@gmail.com>
2026-01-31 14:51:03 +0500

    move to v 1.0.4

--- app.py
+++ app.py
@@ -25,15 +25,21 @@ r = redis.Redis(
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
-# =====================
-# Routes
-# =====================
 @app.route("/")
 def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+# # =====================
+# # Routes
+# # =====================
+
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
+
 
 # @app.route("/verify", methods=["POST"])
 # def verify():
